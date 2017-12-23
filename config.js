1fc6eb9b0bcbd8eea6668bb770f128a203e823f9
ArminderK <arminder.khinda@yahoo.com>
2017-12-23 12:12:59 -0800

    Added gitignore file

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         '7281omuwnvrgFpXY5aMYWBYdP',
-  consumer_secret:      '6hAgoZChQCz8MtIcY6gJTkYOGpnbiQZrJCwfasqhm6QR7iTJUY',
-  access_token:         '922898140440571905-m1j5FO6cYgMQfDXtLb2d0WKTmQiYNQm',
-  access_token_secret:  'uDEPeq63IDVn7dSWZJCvvrZuTgKz8vtN48HItdROmWwKN',
-}
